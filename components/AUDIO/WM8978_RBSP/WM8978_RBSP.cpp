301023e228fc95d373738dca21ce28101356a4af
d-kato <daiki.kato.cj@renesas.com>
2019-06-05 17:24:40 +0900

    Changed to work even when I2S is slave (WM8978)

--- components/AUDIO/WM8978_RBSP/WM8978_RBSP.cpp
+++ components/AUDIO/WM8978_RBSP/WM8978_RBSP.cpp
@@ -42,7 +42,7 @@ WM8978_RBSP::WM8978_RBSP(PinName mosi, PinName miso, PinName sclk, PinName ssel,
     ssif_cfg.sample_freq            = 44100u;
     ssif_cfg.clk_select             = SSIF_CFG_CKS_AUDIO_CLK;
     ssif_cfg.multi_ch               = SSIF_CFG_MULTI_CH_1;
-    ssif_cfg.data_word              = SSIF_CFG_DATA_WORD_32;
+    ssif_cfg.data_word              = SSIF_CFG_DATA_WORD_16;
     ssif_cfg.system_word            = SSIF_CFG_SYSTEM_WORD_32;
     ssif_cfg.bclk_pol               = SSIF_CFG_FALLING;
     ssif_cfg.ws_pol                 = SSIF_CFG_WS_LOW;
@@ -86,70 +86,17 @@ void WM8978_RBSP::power(bool type) {
 }
 
 bool WM8978_RBSP::format(char length) {
-    if (ssif_cfg.slave_mode != false) {
-        uint16_t wl;
-
-        switch (length) {
-            case 16:
-                wl = 0;
-                break;
-            case 20:
-                wl = 1;
-                break;
-            case 24:
-                wl = 2;
-                break;
-            case 32:
-                wl = 3;
-                break;
-            default:
-                return false;
-        }
-        set_register(WM8978_REGADR_AUDIO_IF_CTL, (wl << 5));
-        return true;
-    } else  {
-        if (length != 16) {
-            return false;
-        }
-        return true;
+    if (length != 16) {
+        return false;
     }
+    return true;
 }
 
 bool WM8978_RBSP::frequency(int hz) {
-    if (ssif_cfg.slave_mode != false) {
-        uint16_t sr;
-
-        switch (hz) {
-            case 48000:
-            case 44100:
-                sr = 0;
-                break;
-            case 32000:
-                sr = 1;
-                break;
-            case 24000:
-                sr = 2;
-                break;
-            case 16000:
-                sr = 3;
-                break;
-            case 12000:
-                sr = 4;
-                break;
-            case 8000:
-                sr = 5;
-                break;
-            default:
-                return false;
-        }
-        set_register(0x07, (sr << 1));
-        return true;
-    } else {
-        if (hz != 44100) {
-            return false;
-        }
-        return true;
+    if (hz != 44100) {
+        return false;
     }
+    return true;
 }
 
 // Private Functions
