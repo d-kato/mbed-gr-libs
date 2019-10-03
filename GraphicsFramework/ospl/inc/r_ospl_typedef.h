96f9b419a3e413195479068c4b279509baa61dae
d-kato <daiki.kato.cj@renesas.com>
2019-10-03 19:10:30 +0900

    Compatible with Arm Compiler 6

--- GraphicsFramework/ospl/inc/r_ospl_typedef.h
+++ GraphicsFramework/ospl/inc/r_ospl_typedef.h
@@ -111,7 +111,7 @@ typedef uint8_t        byte_t;
 * @typedef  ssize_t
 * @brief  Signed size type. This is a POSIX specification.
 */
-#if ! defined( __GNUC__ )  ||  defined( __CC_ARM )
+#if ! defined( __GNUC__ )  ||  defined( __CC_ARM ) || (defined (__ARMCC_VERSION) && (__ARMCC_VERSION >= 6010050))
 typedef int  ssize_t;
 #endif
 
