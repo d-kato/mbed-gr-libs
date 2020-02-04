66d5c793e3dfd40f8c27b89a4fe6c09f37ccb768
d-kato <daiki.kato.cj@renesas.com>
2020-02-04 12:52:26 +0900

    Fix cache control

--- dcache-control/dcache-control.c
+++ dcache-control/dcache-control.c
@@ -16,6 +16,9 @@
 
 #include "cmsis.h"
 #include "dcache-control.h"
+#if defined(TARGET_RZ_A2XX)
+#include "r_cache_lld_rza2m.h"
+#endif
 
 void dcache_clean(void * p_buf, uint32_t size) {
     uint32_t start_addr = (uint32_t)p_buf & 0xFFFFFFE0;
@@ -26,6 +29,9 @@ void dcache_clean(void * p_buf, uint32_t size) {
     for (addr = start_addr; addr < end_addr; addr += 0x20) {
         L1C_CleanDCacheMVA((void *)addr);
     }
+#if defined(TARGET_RZ_A2XX)
+    R_CACHE_L2CleanAll();
+#endif
 }
 
 void dcache_invalid(void * p_buf, uint32_t size) {
@@ -37,5 +43,8 @@ void dcache_invalid(void * p_buf, uint32_t size) {
     for (addr = start_addr; addr < end_addr; addr += 0x20) {
         L1C_InvalidateDCacheMVA((void *)addr);
     }
+#if defined(TARGET_RZ_A2XX)
+    R_CACHE_L2InvalidAll();
+#endif
 }
 
